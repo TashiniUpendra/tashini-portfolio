ea29d4b34f08a2ba2fba89575df26550580c82d0
TashiniUpendra <tashiniupendra@gmail.com>
2026-03-09 16:39:03 +0530

    first commit

---
+++ My Site/VitaCare-Pharmacy-Management-System/VitaCare-Pharmacy-Management/VitaCare-Pharmacy-Management/About_Us.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VitaCare_Pharmacy_Management
+{
+    public partial class About_Us : Form
+    {
+        public About_Us()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
